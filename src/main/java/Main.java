633d7abad2ae37090be801ee2c54b6eb19374ca2
DogukanSurme <dogukansurme@eskisehir.edu.tr>
2020-12-15 14:26:33 +0300

    changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,12 +17,10 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Scanner scan = new Scanner(System.in);
-        String alinanUrl = scan.nextLine();
 
         Document doc = null;
         try {
-            doc = Jsoup.connect(alinanUrl).get();
+            doc = Jsoup.connect(args[0]).get();
 
         } catch (IOException e) {
             e.printStackTrace();
